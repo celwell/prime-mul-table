6ccdc2e341ea771d50b64913312fbea6d73b202a
Christopher Elwell <elwell.christopher@gmail.com>
2019-01-09 18:57:22 -0800

    add codox

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.4.1"]]
+  :plugins [[lein-codox "0.10.5"]]
   :main ^:skip-aot prime-mul-table.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :codox {:output-path "codox"})
